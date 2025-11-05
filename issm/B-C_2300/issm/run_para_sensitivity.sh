4fd6e85760d3fde595200c380a4a496e1fe6bd1c
Tim <tha111@sfu.ca>
2025-11-05 17:27:19 -0500

    Update runs

---
+++ issm/B-C_2300/issm/run_para_sensitivity.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=def-gflowers
+#SBATCH --job-name=para-sensitivity
+#SBATCH --time=0-01:00
+#SBATCH --mem=4G
+#SBATCH --output=run_para_sensitivity.out
+
+source ~/SFU-code/antarctic-glads/venv/bin/activate
+
+python -u run_para_sensitivity.py
